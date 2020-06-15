d6953edbf8681aa51aef6f5ededc45cf9d897a50
LeahHersh <61760903+LeahHersh@users.noreply.github.com>
2020-06-15 18:36:43 -0400

    Add files via upload
    View Controller for uncopied project

--- ViewController.swift
+++ ViewController.swift
@@ -1,11 +1,21 @@
-
+//
+//  ViewController.swift
+//  Ultimate Sloths Quiz
+//
+//  Created by admin on 5/18/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
 
 import UIKit
 
 class ViewController: UIViewController {
+  
 
-    @IBOutlet weak var questionLabel: UILabel!
+
     
+    
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
     
     @IBOutlet weak var answer0: UIButton!
     @IBOutlet weak var answer1: UIButton!
@@ -14,106 +24,137 @@ class ViewController: UIViewController {
     
     
     
-    @IBOutlet weak var progressLabel: UILabel!
     
+    @IBAction func handleSubmitAnswer0(_ sender: Any) {
+        checkAnswer(choiceNo: 0)
 
-    @IBAction func submitAnswer0(_ sender: Any) {
-          checkAnswer(idx: 0)
     }
     
-    @IBAction func submitAnswer1(_ sender: Any) {
-          checkAnswer(idx: 1)
+    @IBAction func handleSubmitAnswer1(_ sender: Any) {
+        checkAnswer(choiceNo: 1)
     }
     
-    @IBAction func submitAnswer2(_ sender: Any) {
-          checkAnswer(idx: 2)
+    @IBAction func handleSubmitAnswer2(_ sender: Any) {
+        checkAnswer(choiceNo: 2)
     }
     
-    @IBAction func submitAnswer3(_ sender: Any) {
-          checkAnswer(idx: 3)
-    }
-    
-    
-    
+    @IBAction func handleSubmitAnswer3(_ sender: Any) {
+        checkAnswer(choiceNo: 3)
 
-var currentQuestion: Question?
-    var currentQuestionPosition: Int = 0
-    var noCorrect: Int = 0
+    }
     
-    func checkAnswer(idx: Int) {
-        if (currentQuestion!.correctAnswer == idx) {
+    
+    var Number:Int=0
+    var currentQuestion: Question?
+    var currentQuestionPosition:Int=0
+    var noCorrect:Int=0
+
+    
+    func checkAnswer(choiceNo:Int){
+        if(choiceNo == currentQuestion!.correctAnswer){
             noCorrect += 1
-        }
-        loadNextQuestion()
+            loadNextQuestion()
+        }else{
+    loadNextQuestion()
+    }
     }
     
-    func setQuestions() {
-        questionLabel.text = currentQuestion!.question
-        answer0.setTitle(currentQuestion!.answers[0], for: .normal)
-        answer1.setTitle(currentQuestion!.answers[1], for: .normal)
-        answer2.setTitle(currentQuestion!.answers[2], for: .normal)
-        answer3.setTitle(currentQuestion!.answers[3], for: .normal)
-        progressLabel.text = "Question: \(currentQuestionPosition + 1)/ \(questions.count)"
-    }
- 
-    func loadNextQuestion() {
-        if (currentQuestionPosition + 1 < questions.count) {
-            currentQuestionPosition += 1
-            currentQuestion = questions[currentQuestionPosition]
-            setQuestions()
-        } else {
-            performSegue(withIdentifier: "sgShowResults", sender: nil)
+    
+    func setQuestion() {
+
+      questionLabel.text = currentQuestion!.question
+      answer0.setTitle(currentQuestion!.answers[0], for: .normal)
+      answer1.setTitle(currentQuestion!.answers[1], for: .normal)
+      answer2.setTitle(currentQuestion!.answers[2], for: .normal)
+      answer3.setTitle(currentQuestion!.answers[3], for: .normal)
+      progressLabel.text = "Question \(currentQuestionPosition + 1) of \(questions.count)"
         }
+    
+
+    func loadNextQuestion(){
+         if ((currentQuestionPosition + 1) == questions.count){
+         performSegue(withIdentifier: "sgShowResults", sender: nil)
+      
+         } else{
+             currentQuestionPosition += 1
+             currentQuestion = questions[currentQuestionPosition]
+             setQuestion()
+       
+
+         }
+     }
+    
+    
+     struct Question {
+         let question: String
+         let answers: [String]
+         let correctAnswer: Int
     }
-    struct Question {
-        let question: String
-        let answers: [String]
-        let correctAnswer: Int
-    }
+    
+    
     var questions: [Question] = [
         Question(
-            question: "What is Taylor Swift's first album?",
-            answers: ["1989","Taylor Swift", "Speak Now", "Fearless"],
+            question: "Where do sloths live?",
+            answers: ["South America", "North America", "Central America", "1 and 3"],
+            correctAnswer: 3),
+        Question(
+            question: "Sloth's relationship with the algae in their furr is:",
+            answers: ["Parasitic", "Symbiotic", "Commensalitic", "Nonexistent"],
             correctAnswer: 1),
         Question(
-            question: "What is Taylor Swift's second album?",
-            answers: ["1989", "Red", "Taylor Swift", "Fearless"],
-            correctAnswer: 3),
-        Question(
-            question: "What is Taylor Swift's third album?",
-            answers: ["Reputation", "Red", "Fearless", "Speak Now"],
-            correctAnswer: 3),
-        Question(
-            question: "What is Taylor Swift's fourth album?",
-            answers: ["1989", "Taylor Swift", "Red", "Fearless"],
+            question: "Sloths' lives become at risk through which typically safe process?",
+            answers: ["Digestion", "Shedding", "Defacation", "Camouflage"],
             correctAnswer: 2),
         Question(
-            question: "What is Taylor Swift's fifth album?",
-            answers: ["Fearless", "1989", "Reputation", "Red"],
-            correctAnswer: 1),
+               question: "How many toes does a two-toed sloth have?",
+               answers: ["2", "3", "4", "5"],
+               correctAnswer: 1),
         Question(
-            question: "What is Taylor Swift's sixth album?",
-            answers: ["1989", "Reputation", "Red", "Fearless"],
-            correctAnswer: 1)
+               question: "Shocking I know. How many fingers does a two-toed sloth have?",
+               answers: ["2", "3", "4", "5"],
+               correctAnswer: 0)
     ]
-    
+
     
 
     override func viewDidLoad() {
+     
         super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
         currentQuestion = questions[0]
-        setQuestions() //this gives the value to the answers
+        setQuestion()
+        
     }
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if(segue.identifier == "sgShowResults") {
-            print("begun")
-           let vc = segue.destination as!ResultsViewController
-            vc.noCorrect = noCorrect
-            vc.total = questions.count
-        }
+    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      if(segue.identifier == "sgShowResults") {
+        let vc = segue.destination as! ResultsViewController
+        vc.noCorrect = noCorrect
+        vc.total = questions.count
+      }
+        
+
+        
     }
+    
+    
+ 
+    
+
+
+
+
+    
+
+
+    
 
 
 
 
 }
+
+
+
